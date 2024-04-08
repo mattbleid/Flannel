b5549327b8f4c95707d22e2eceb131d15e560d8b
Noah McQuerter <nm700@live.missouristate.edu>
2024-04-07 20:46:19 -0500

    modified:   connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,39 @@
 <?php
-$user = filter_input(INPUT_POST, 'nameField');
-$pass = filter_input(INPUT_POST, 'passField');
+$user = filter_input(INPUT_POST, 'username');
+$pass = filter_input(INPUT_POST, 'password');
 
 if (!empty($user))
 {
-    echo $user;
+    if (!empty($pass))
+    {
+        $host = "localhost";
+        $dbusername = "root";
+        $dbpassword = "";
+        $dbname = "flannelemotiondata";
+
+        // Create connection
+        $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+        
+        if (mysqli_connect_error())
+        {
+            die('Connect Error ('. mysqli_connect_error() .') '
+            . mysqli_connect_error());
+        }
+        else
+        {
+            $sql = "INSERT INTO account (username, password)
+            values ('$username', '$password')";
+        }
+    }
+    else
+    {
+        echo "Password shouldn't be empty";
+        die();
+    }
+}
+else
+{
+    echo "Username shouldn't be empty";
+    die();
 }
 ?>
